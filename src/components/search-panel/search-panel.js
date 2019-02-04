df238b2d6437ee4e8095bc784931acd4d4d652f4
Alexander Makarof <makarofalex@gmail.com>
2019-02-04 23:10:04 +0300

    fixed placeholder

--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -18,7 +18,7 @@ export default class SearchPanel extends Component {
         <input
           type="text"
           className="form-control search-input "
-          placeholder="type to search"
+          placeholder="Type to search"
           aria-label="Username"
           onChange={this.onSearchChange}
           value={this.state.term}/>
